e8d2a3708dcf674b826112da9243b79d8ceed797
Alberto Casagrande <acasagrande@units.it>
2023-07-10 14:26:49 +0200

    Added the tissue sampler notion - implemented `RectangleSampler` to traverse all the tissue cells in a hyper-rectangle

---
+++ include/sampler.hpp
@@ -0,0 +1,265 @@
+/**
+ * @file sampler.hpp
+ * @author Alberto Casagrande (acasagrande@units.it)
+ * @brief Define classes to sample cells in a tissue
+ * @version 0.1
+ * @date 2023-07-10
+ * 
+ * @copyright Copyright (c) 2023
+ * 
+ * MIT License
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#ifndef __RACES_SAMPLER__
+#define __RACES_SAMPLER__
+
+#include <cstdint>
+#include <vector>
+
+#include "tissue.hpp"
+
+namespace Races
+{
+
+/**
+ * @brief The basic class for tissue cell samplers 
+ */
+struct BasicSampler
+{
+    struct const_iterator {
+    };
+
+    /**
+     * @brief Get the initial sampled cell iterator
+     * 
+     * This method must be over-loaded.
+     * 
+     * @return the initial sampled cell iterator
+     * @throws std::runtime_error
+     */
+    const_iterator begin() const;
+
+    /**
+     * @brief Get the final sampled cell iterator
+     * 
+     * This method must be over-loaded.
+     * 
+     * @return the final sampled cell iterator
+     * @throws std::runtime_error
+     */
+    const_iterator end() const;
+};
+
+/**
+ * @brief A class to extract the tissue cells in a hyper-rectangle
+ */
+class RectangleSampler : public BasicSampler
+{
+    Tissue const& tissue;           
+
+    PositionInTissue lower_corner;  //!< The lower corner in the sampler hyper-rectangle
+    PositionInTissue upper_corner;  //!< The upper corner in the sampler hyper-rectangle
+
+public:
+    class const_iterator
+    {
+        const RectangleSampler* sampler;
+
+        PositionInTissue pos;
+
+        /**
+         * @brief A constructor
+         * 
+         * @param sampler is the sampler whose cells are traversed by the iterator
+         * @param position is the position in the tissue of the new iterator
+         */
+        const_iterator(const RectangleSampler* sampler, const PositionInTissue& position);
+    public:
+        using difference_type   =   std::ptrdiff_t;
+        using value_type        =   Cell;
+        using pointer           =   const Cell*;
+        using reference         =   const Cell&;
+        using iterator_category =   std::bidirectional_iterator_tag;
+
+        /**
+         * @brief An empty construtor
+         */
+        const_iterator();
+
+        /**
+         * @brief Reference operator
+         * 
+         * @return a reference to the species pointer by the iterator 
+         */
+        inline reference operator*() const 
+        { 
+            return sampler->tissue(pos);
+        }
+
+        /**
+         * @brief Pointer operator
+         * 
+         * @return a pointer to the species pointer by the iterator 
+         */
+        inline pointer operator->() 
+        {
+            return &(static_cast<const CellInTissue&>(sampler->tissue(pos)));
+        }
+
+        /**
+         * @brief The prefix increment
+         * 
+         * @return a reference to the updated object
+         */
+        const_iterator& operator++();
+
+        /**
+         * @brief The postfix increment
+         * 
+         * @return a copy of the original object
+         */
+        inline const_iterator operator++(int)
+        {
+            RectangleSampler::const_iterator copy(*this);
+
+            this->operator++();
+
+            return copy;
+        }
+
+        /**
+         * @brief The prefix decrement
+         * 
+         * @return a reference to the updated object
+         */
+        const_iterator& operator--();
+
+        /**
+         * @brief The postfix decrement
+         * 
+         * @return a copy of the original object
+         */
+        inline const_iterator operator--(int)
+        {
+            RectangleSampler::const_iterator copy(*this);
+
+            this->operator--();
+
+            return copy;
+        }
+
+        /**
+         * @brief Test whether two iterators are the same
+         * 
+         * @param a is the first iterator to compare
+         * @param b is the second iterator to compare
+         * @return `true` if and only if the two iterators 
+         *      refer to the same object
+         */
+        friend inline bool operator==(const const_iterator& a, const const_iterator& b)
+        { 
+            return (a.pos == b.pos) && (a.sampler == b.sampler); 
+        }
+
+        friend class RectangleSampler;
+    };
+
+    /**
+     * @brief A hyper-rectangle sampler constructor
+     * 
+     * @param tissue is the tissue to sample
+     * @param lower_corner is the hyper-rectangle lower corner
+     * @param upper_corner is the hyper-rectangle upper corner
+     */
+    RectangleSampler(const Tissue& tissue, const PositionInTissue& lower_corner, const PositionInTissue& upper_corner);
+
+    /**
+     * @brief A cuboid sampler constructor
+     * 
+     * @param tissue is the tissue to sample
+     * @param lower_corner is the hyper-rectangle lower corner 
+     * @param x_size is the hyper-rectangle size along the x-axis
+     * @param y_size is the hyper-rectangle size along the y-axis
+     * @param z_size is the hyper-rectangle size along the z-axis
+     */
+    RectangleSampler(const Tissue& tissue, const PositionInTissue& lower_corner, 
+                     const AxisSize& x_size, const AxisSize& y_size, const AxisSize& z_size);
+
+    /**
+     * @brief A rectangle sampler constructor
+     * 
+     * @param tissue is the tissue to sample
+     * @param lower_corner is the hyper-rectangle lower corner 
+     * @param x_size is the hyper-rectangle size along the x-axis
+     * @param y_size is the hyper-rectangle size along the y-axis
+     */
+    RectangleSampler(const Tissue& tissue, const PositionInTissue& lower_corner, 
+                     const AxisSize& x_size, const AxisSize& y_size);
+
+    /**
+     * @brief Get the initial sampled cell iterator
+     * 
+     * @return the initial sampled cell iterator
+     */
+    const_iterator begin() const;
+
+    /**
+     * @brief Get the final sampled cell iterator
+     * 
+     * @return the final sampled cell iterator
+     */
+    const_iterator end() const;
+
+    /**
+     * @brief Test whether two rectangle samplers are the same
+     * 
+     * @param a is the first rectangle sampler to compare
+     * @param b is the second rectangle sampler to compare
+     * @return `true` if and only if the two rectangle samplers 
+     *      refer to the same object
+     */
+    friend inline bool operator==(const RectangleSampler& a, const RectangleSampler& b)
+    { 
+        return ((&a.tissue==&b.tissue)
+                && (a.lower_corner==b.lower_corner)
+                && (a.upper_corner==b.upper_corner));
+    }
+
+    friend RectangleSampler::const_iterator;
+};
+
+
+/**
+ * @brief Test whether two iterators differs
+ * 
+ * @param a is the first iterator to compare
+ * @param b is the second iterator to compare
+ * @return `true` if and only if the two iterators 
+ *      do not refer to the same object
+ */
+inline bool operator!=(const RectangleSampler::const_iterator& a, const RectangleSampler::const_iterator& b)
+{ 
+    return !(a==b); 
+}
+
+}
+
+#endif // __RACES_SAMPLER__
---
+++ src/sampler.cpp
@@ -0,0 +1,218 @@
+/**
+ * @file sampler.cpp
+ * @author Alberto Casagrande (acasagrande@units.it)
+ * @brief Tissue sampler class implementation
+ * @version 0.1
+ * @date 2023-07-10
+ * 
+ * @copyright Copyright (c) 2023
+ * 
+ * MIT License
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#include "sampler.hpp"
+
+namespace Races
+{
+
+BasicSampler::const_iterator BasicSampler::begin() const
+{
+    throw std::runtime_error("The BasicSampler class must be inherited");
+}
+
+BasicSampler::const_iterator BasicSampler::end() const
+{
+    throw std::runtime_error("The BasicSampler class must be inherited");
+}
+
+RectangleSampler::const_iterator::const_iterator(const RectangleSampler* sampler, const PositionInTissue& position):
+    sampler(sampler), pos(position)
+{
+    if (!sampler->tissue(pos).has_driver_mutations()) {
+        this->operator++();
+    }
+}
+
+RectangleSampler::const_iterator::const_iterator():
+    sampler(nullptr)
+{
+}
+
+template<typename T>
+bool increase_and_update(T& value, const T& min, const T& max)
+{
+    if (value >= max) {
+        value = min;
+
+        return true;
+    } 
+    
+    ++value;
+    return false;
+}
+
+void set_to_invalid_position(PositionInTissue& pos, const Tissue& tissue)
+{
+    auto sizes = tissue.size();
+    pos.x = sizes[0];
+    pos.y = sizes[1];
+    if (sizes.size()==2) {
+        pos.z = 0;
+    } else {
+        pos.z = sizes[2];
+    }
+}
+
+RectangleSampler::const_iterator& RectangleSampler::const_iterator::operator++()
+{
+    if (sampler==nullptr) {
+        return *this;
+    }
+
+    bool found_a_cell{false};
+    while (!found_a_cell) {
+        if (increase_and_update(pos.z, sampler->lower_corner.z, sampler->upper_corner.z)) {
+            if (increase_and_update(pos.y, sampler->lower_corner.y, sampler->upper_corner.y)) {
+                if (increase_and_update(pos.x, sampler->lower_corner.x, sampler->upper_corner.x)) {
+                    set_to_invalid_position(pos, sampler->tissue);
+
+                    return *this;
+                }
+            }
+        }
+
+        found_a_cell = sampler->tissue(pos).has_driver_mutations();
+    }
+
+    return *this;
+}
+
+template<typename T>
+bool decrease_and_update(T& value, const T& min, const T& max)
+{
+    if (value <= min) {
+        value = max;
+
+        return true;
+    } 
+    
+    --value;
+    return false;
+}
+
+RectangleSampler::const_iterator& RectangleSampler::const_iterator::operator--()
+{
+    if (sampler==nullptr) {
+        return *this;
+    }
+
+    bool found_a_cell{false};
+    while (found_a_cell) {
+        if (decrease_and_update(pos.z, sampler->lower_corner.z, sampler->upper_corner.z)) {
+            if (decrease_and_update(pos.y, sampler->lower_corner.y, sampler->upper_corner.y)) {
+                if (decrease_and_update(pos.x, sampler->lower_corner.x, sampler->upper_corner.x)) {
+                    set_to_invalid_position(pos, sampler->tissue);
+        
+                    return *this;
+                }
+            }
+        }
+
+        found_a_cell = sampler->tissue(pos).has_driver_mutations();
+    }
+
+    return *this;
+}
+
+RectangleSampler::RectangleSampler(const Tissue& tissue, const PositionInTissue& lower_corner, 
+                                   const PositionInTissue& upper_corner):
+    tissue(tissue), lower_corner(lower_corner), upper_corner(upper_corner)
+{
+    if (tissue.num_of_dimensions()==2 && lower_corner.z!=0) { 
+        throw std::domain_error("The tissue is a 2D space. The lower corner must be a 2D position.");
+    }
+    if (tissue.num_of_dimensions()==2 &&  upper_corner.z!=0) { 
+        throw std::domain_error("The tissue is a 2D space. The upper corner must be a 2D position.");
+    }
+
+    if (!tissue.is_valid(lower_corner)) {
+        throw std::domain_error("The lower corner is not valid for the tissue");
+    }
+
+    if (!tissue.is_valid(upper_corner)) {
+        throw std::domain_error("The upper corner is not valid for the tissue");
+    }
+}
+
+RectangleSampler::RectangleSampler(const Tissue& tissue, const PositionInTissue& lower_corner, 
+                                   const AxisSize& x_size, const AxisSize& y_size, const AxisSize& z_size):
+    tissue(tissue), lower_corner(lower_corner), 
+    upper_corner(lower_corner.x+x_size-1,lower_corner.y+y_size-1,lower_corner.z+z_size-1)
+{
+    if (tissue.num_of_dimensions()==2) { 
+        throw std::domain_error("The tissue is a 2D space. The sampler "
+                                "hyper-rectangle must be a 2D rectangle.");
+    }
+
+    if (!tissue.is_valid(lower_corner)) {
+        throw std::domain_error("The lower corner is not valid for the tissue");
+    }
+
+    if (!tissue.is_valid(upper_corner)) {
+        throw std::domain_error("The rectangle sizes are not compatible with the tissue sizes");
+    }
+}
+
+RectangleSampler::RectangleSampler(const Tissue& tissue, const PositionInTissue& lower_corner, 
+                                   const AxisSize& x_size, const AxisSize& y_size):
+    tissue(tissue), lower_corner(lower_corner), 
+    upper_corner(lower_corner.x+x_size-1,lower_corner.y+y_size-1)
+{
+    if (tissue.num_of_dimensions()==3) { 
+        throw std::domain_error("The tissue is a 3D space. The sampler "
+                                "hyper-rectangle sizes must be 3 in number.");
+    }
+    if (lower_corner.z!=0) { 
+        throw std::domain_error("The lower corner must be a 2D position.");
+    }
+
+    if (!tissue.is_valid(lower_corner)) {
+        throw std::domain_error("The lower corner is not valid for the tissue");
+    }
+
+    if (!tissue.is_valid(upper_corner)) {
+        throw std::domain_error("The rectangle sizes are not compatible with the tissue sizes");
+    }
+}
+
+RectangleSampler::const_iterator RectangleSampler::begin() const
+{
+    return const_iterator(this, lower_corner);
+}
+
+RectangleSampler::const_iterator RectangleSampler::end() const
+{
+    const_iterator it(this, upper_corner);
+
+    return ++it;
+}
+
+}
