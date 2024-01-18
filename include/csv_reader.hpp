ea5a5a33dd15b9932dd40dfcff0d9cb03f0e5d31
Alberto Casagrande <acasagrande@units.it>
2024-01-18 10:25:19 +0100

    Implemented `CSVReader`

---
+++ include/csv_reader.hpp
@@ -0,0 +1,222 @@
+/**
+ * @file csv_reader.hpp
+ * @author Alberto Casagrande (alberto.casagrande@uniud.it)
+ * @brief Defines a class to read CSV
+ * @version 0.1
+ * @date 2024-01-18
+ *
+ * @copyright Copyright (c) 2023-2024
+ *
+ * MIT License
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#ifndef __RACES_CSV_READER__
+#define __RACES_CSV_READER__
+
+#include <map>
+#include <vector>
+#include <fstream>
+
+namespace Races
+{
+
+namespace IO
+{
+
+/**
+ * @brief A class to read CSV
+ */
+class CSVReader
+{
+    const std::filesystem::path filename;  //!< The CSV filename
+
+    const char col_sep;             //!< The column separator
+
+    std::map<std::string, size_t> columns; //!< the column positions
+
+    std::streampos first_row_pos;   //!< the position in the file of the first data row
+
+public:
+    /**
+     * @brief A class to iterate over CSV rows
+     */
+    class const_iterator;
+
+    /**
+     * @brief A class representing a CSV row
+     */
+    class CSVRow
+    {
+        std::vector<std::string> fields;  //!< The row fields
+
+        CSVRow(const std::string& row, const char& column_separator=',');
+    public:
+        /**
+         * @brief The empty constructor
+         */
+        CSVRow();
+
+        const std::string& get_field(const size_t& index) const;
+
+        inline size_t size() const
+        {
+            return fields.size();
+        }
+
+        friend class const_iterator;
+    };
+
+    class const_iterator
+    {
+        const CSVReader *reader;    //!< A pointer to the reader
+        std::ifstream ifs;          //!< The CSV file stream
+        CSVRow curr_row;            //!< The row of the current iterator
+        bool eof;                   //!< A Boolean flag to denote when eof has been reached
+
+        /**
+         * @brief Get the reading position on the CSV file stream
+         * 
+         * @return the reading position on the CSV file stream
+         */
+        inline std::streampos tellg()
+        {
+            return ifs.tellg();
+        }
+
+        /**
+         * @brief Read a CSV row
+         * 
+         * @param row is the object in which the read row must be stored
+         * @return `true` if and only if the operation succeeds 
+         */
+        bool get_row(CSVRow& row);
+
+        /**
+         * @brief The constructor
+         * 
+         * @param reader is a constant pointer to the reader
+         * @param off is the offset from the `way` position
+         * @param way is the file position from which the offset `off`
+         *          must be accounted
+         */
+        const_iterator(const CSVReader* reader, const std::streamoff& off=0,
+                       const std::ios_base::seekdir& way=std::ios_base::beg);
+    
+    public:
+
+        /**
+         * @brief Move to the next row
+         * 
+         * @return a reference to the updated object
+         */
+        const_iterator& operator++();
+
+        /**
+         * @brief Dereference the iterator
+         * 
+         * @return a constant reference to the referenced CSV row
+         */
+        inline const CSVRow& operator*() const
+        {
+            if (eof) {
+                throw std::runtime_error("No rows available: EOF have been reached.");
+            }
+            return curr_row;
+        }
+
+        /**
+         * @brief Get the pointer to the object referenced by the iterator
+         * 
+         * @return a constant pointer to the referenced CSV row
+         */
+        inline const CSVRow* operator->() const
+        {
+            if (eof) {
+                throw std::runtime_error("No rows available: EOF have been reached.");
+            }
+            return &curr_row;
+        }
+
+        /**
+         * @brief Check whether two iterator refer to the same row
+         * 
+         * @param other is a constant iterator over a CSV reader
+         * @return `true` if and only if the current iterator and 
+         *           `other` are refering to the same CSV row
+         */
+        inline bool operator==(const_iterator& other) 
+        { 
+            return reader == other.reader && ifs.tellg() == other.ifs.tellg();
+        }
+
+        /**
+         * @brief Check whether two iterator refer to different rows
+         * 
+         * @param other is a constant iterator over a CSV reader
+         * @return `false` if and only if the current iterator and 
+         *           `other` are refering to the same CSV row
+         */
+        inline bool operator!=(const_iterator& other) 
+        { 
+            return !(*this == other);
+        }
+
+        friend class CSVReader;
+    };
+
+    /**
+     * @brief A constructor
+     * 
+     * @param filename is the path of the CSV file to be read
+     * @param has_header is a Boolean flag to enable/disable header in CSV file
+     * @param column_separator is the character used to separate columns
+     */
+    CSVReader(const std::filesystem::path& filename, const bool has_header=true,
+              const char column_separator=',');
+
+    /**
+     * @brief Get an iterator refering to the first data row in the CSV
+     * 
+     * @return an iterator refering to the first data row in the CSV 
+     */
+    inline const_iterator begin() const
+    {
+        return const_iterator(this, first_row_pos);
+    }
+
+    /**
+     * @brief Get an iterator refering to the end of the CSV
+     * 
+     * @return an iterator refering to the end of the CSV
+     */
+    inline const_iterator end() const
+    {
+        return const_iterator(this, 0, std::ios_base::end);
+    }
+
+    friend class const_iterator;
+};
+
+}   // IO
+
+}   // Races
+
+#endif // __RACES_CSV_READER__
---
+++ src/csv_reader.cpp
@@ -0,0 +1,125 @@
+/**
+ * @file csv_reader.cpp
+ * @author Alberto Casagrande (alberto.casagrande@uniud.it)
+ * @brief Implements a class to read CSV
+ * @version 0.1
+ * @date 2024-01-18
+ *
+ * @copyright Copyright (c) 2023-2024
+ *
+ * MIT License
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#include <sstream>
+
+#include "csv_reader.hpp"
+
+namespace Races
+{
+
+namespace IO
+{
+
+CSVReader::CSVRow::CSVRow():
+    fields()
+{}
+
+CSVReader::CSVRow::CSVRow(const std::string& row, const char& column_separator)
+{
+    std::string field;
+    std::stringstream oss_line(row);
+
+    while(std::getline(oss_line, field, column_separator)) {
+        fields.push_back(field);
+    }
+}
+
+const std::string& CSVReader::CSVRow::get_field(const size_t& index) const
+{
+    if (index<fields.size()) {
+        return fields[index];
+    }
+
+    throw std::out_of_range("The CSV row contains " 
+                            + std::to_string(fields.size())
+                            + " fields. Requested field in position "
+                            + std::to_string(index));
+}
+
+bool CSVReader::const_iterator::get_row(CSVReader::CSVRow& row)
+{
+    std::string line;
+
+    if (!getline(ifs, line)) {
+        return false;
+    }
+
+    row = CSVRow(line, reader->col_sep);
+
+    return true;
+}
+
+CSVReader::const_iterator::const_iterator(const CSVReader* reader, const std::streamoff& off, 
+                                          const std::ios_base::seekdir& way):
+    reader(reader), ifs(reader->filename), eof(way==std::ios_base::end)
+{
+    ifs.seekg(off, way);
+    if (!eof) {
+        get_row(curr_row);
+    }
+}
+
+CSVReader::const_iterator& CSVReader::const_iterator::operator++()
+{
+    eof = !get_row(curr_row);
+
+    return *this;
+}
+
+CSVReader::CSVReader(const std::filesystem::path& filename, const bool has_header,
+                     const char column_separator):
+    filename(filename), col_sep(column_separator), first_row_pos(0)
+{
+    if (has_header) {
+        std::ifstream ifs(filename);
+        std::string line;
+        
+        if (!getline(ifs, line)) {
+            throw std::runtime_error("The file does not have a header row.");
+        }
+
+        std::string column_name;
+        std::stringstream oss_line(line);
+
+        size_t i{0};
+        while(std::getline(oss_line, column_name, col_sep)) {
+            columns[column_name] = i;
+
+            ++i;
+        }
+
+        first_row_pos = ifs.tellg();
+    }
+}
+
+}   // IO
+
+}   // Races
